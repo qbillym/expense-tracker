40f66d7feb077ce4584cbc9af75e05f5c8fd0009
billy <mucyobilly3@gmail.com>
2026-05-07 10:12:46 +0200

    fix seeder and start script

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 php-fpm -D
+php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
+php artisan db:seed --force
 nginx -g "daemon off;"
